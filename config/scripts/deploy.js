29565693358da926b1a8b89ec9b84aff8b593d75
Rijul Gupta <rijulg@gmail.com>
2018-09-30 17:43:25 +1000

    Updated themekit config file address resolution method

--- config/scripts/deploy.js
+++ config/scripts/deploy.js
@@ -1,6 +1,7 @@
 const chalk = require('chalk');
 const shell = require('shelljs');
 const args = require('minimist')(process.argv.slice(2));
+const paths = require('../paths');
 
 process.env.NODE_ENV = 'production';
 const theme = process.env.THEMEKIT_ALIAS ? process.env.THEMEKIT_ALIAS : 'theme';
@@ -8,7 +9,7 @@ const theme = process.env.THEMEKIT_ALIAS ? process.env.THEMEKIT_ALIAS : 'theme';
 const method = args.m ? args.m : 'upload';
 const force = args.f === 'y' ? '-f' : '';
 const buildFolder = 'build';
-const config = '../../themekit.yml';
+const config = paths.themeKitConfig;
 
 if (shell.exec(`cd ${buildFolder} && ${theme} ${method} ${force} -c ${config} -e ${process.env.NODE_ENV}`).code !== 0) {
     console.log(chalk.red("Theme deployment failed"));
